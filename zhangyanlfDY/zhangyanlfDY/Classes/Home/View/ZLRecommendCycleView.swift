b452956dbf2e2632a0af1babf598d37dfa156d4d
Zhylf <zhyl529@163.com>
2018-04-02 12:15:52 +0800

    添加定时器实现无限轮播

--- zhangyanlfDY/zhangyanlfDY/Classes/Home/View/ZLRecommendCycleView.swift
+++ zhangyanlfDY/zhangyanlfDY/Classes/Home/View/ZLRecommendCycleView.swift
@@ -14,10 +14,21 @@ private let zCycleCellID = "zCycleCellID"
 class ZLRecommendCycleView: UIView {
     
     ///MARK: -  定义属性
+    var cycleTimer : Timer?
+    
     var cycleModels : [ZLCycleModel]? {
         didSet {
+            //1.刷新 collectionView
             collectionView.reloadData()
-            pageControl.numberOfPages = (cycleModels?.count)!
+            //2.设置pageControl numberOfPages
+            pageControl.numberOfPages = cycleModels?.count ?? 0
+            //3.默认设置滚动到中间某一位置
+            let indexPath = NSIndexPath(item: (cycleModels?.count ?? 0) * 10, section: 0)
+            collectionView.scrollToItem(at: indexPath as IndexPath, at: .left, animated: false)
+            //4.
+            removeCycleTimer()
+            addCycleTimer()
+            
         }
     }
     
@@ -67,7 +78,7 @@ extension ZLRecommendCycleView {
 //MARK: - 遵守UICollectionViewDataSource代理
 extension ZLRecommendCycleView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cycleModels?.count ?? 0
+        return (cycleModels?.count ?? 0) * 10000
         
     }
     
@@ -76,7 +87,7 @@ extension ZLRecommendCycleView: UICollectionViewDataSource {
         
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: zCycleCellID, for: indexPath) as! ZLCollectionCycleCell
-        cell.cycleModel = cycleModels![indexPath.item]
+        cell.cycleModel = cycleModels![indexPath.item % (cycleModels?.count)!]
        
         return cell
         
@@ -88,7 +99,41 @@ extension ZLRecommendCycleView: UICollectionViewDelegate {
         //1.获取滚动的偏移量
         let offsetX = scrollView.contentOffset.x + scrollView.bounds.width * 0.5
         //2.计算pageCotroller 的 currentIndex
-        pageControl.currentPage = Int(offsetX / scrollView.bounds.width)
+        pageControl.currentPage = Int(offsetX / scrollView.bounds.width) % (cycleModels?.count ?? 1)
+        
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeCycleTimer()
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        addCycleTimer()
+    }
+}
+
+//MARK: - 对定时器的操作方法
+extension ZLRecommendCycleView {
+    private func addCycleTimer () {
+        cycleTimer = Timer(timeInterval: 3.0, target: self, selector: #selector(scrollToIndex), userInfo: nil, repeats: true)
+        RunLoop.main.add(cycleTimer!, forMode: RunLoopMode.commonModes)
+        
+    }
+    
+    private func removeCycleTimer () {
+        cycleTimer?.invalidate() //从运行循环中移除
+        cycleTimer = nil
+    }
+    @objc private func scrollToIndex() {
+        //1.获取滚动的偏移量
+        let currretOffsetX = collectionView.contentOffset.x
+        let offsetX = currretOffsetX + collectionView.bounds.width
+        
+        //2.滚动到位置
+        
+        collectionView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
+        
         
     }
 }
+
